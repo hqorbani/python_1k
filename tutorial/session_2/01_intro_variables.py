2be8607c109e43df365bf862f9017cf1f6c93dd7
Hamzeh Qorbani <h.qorbani7@gmail.com>
2024-04-15 17:20:35 -0700

    session_2: define variables

---
+++ tutorial/session_2/01_intro_variables.py
@@ -0,0 +1,5 @@
+name = "Hamzeh" 
+x = "Mehrdad"
+age = 39
+print('Hello' , name ,"and", x)
+print(name , "I know your age. you are" , age , "years old." )
---
+++ tutorial/session_2/02_get_data.py
@@ -0,0 +1,5 @@
+name = input(" lotfan name khod ra vared konid: ") # string str
+birth_year = int(input("please enter your birth year: ")) # interger int
+# print("your name is" , "name")
+print("your name is" , name)
+print("you are" , 2024 - birth_year , "years old.")
