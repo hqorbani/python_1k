6e260c717289032d25d9b5e1748e18596194bf8e
Hamzeh Qorbani <h.qorbani7@gmail.com>
2024-05-01 11:54:45 -0700

    some tips to write clean code

---
+++ practice/session_5_guide/some_tps.py
@@ -0,0 +1,21 @@
+# when you assign some values to a variable by [or] python assigns the first one to your variable
+sex = "male" or "female"  # print(sex) => male
+
+# when you assign some values to a variable by [and] python assigns the last one to your variable
+sex = "male" and "female"  # print(sex) => female
+
+# if you want get gender from user you need to use input command:
+sex = input("please enter the user gender(male/female): ")
+
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# To assign a value to a variable you have to use equal sign between variable name and value.
+material status: "y" or "n"  # this is not true
+
+material_status = "y" # this is true
+#consider y = 20 or any other number
+y = 20
+# The correct form of: if y=="male" or ">40" is:
+if y == "male" or y > 40 :
+
+# The correct form of: if y== "male" or >"60" is:
+if y == "male" or y > 60 :
